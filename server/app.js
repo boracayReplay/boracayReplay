240d55e4b66a57e9fca34916bb84d2528551e079
free-lunch <freelunch@zigbang.com>
2017-02-04 13:22:45 +0900

    Add new blue-print
    - Add react
    - Add express
    - Add configs

--- server/app.js
+++ server/app.js
@@ -2,22 +2,20 @@
  * Created by free-lunch on 2017. 2. 3..
  */
 
+var firebase = require("firebase");
 var express = require('express')
   , app = express()
   , bodyParser = require('body-parser')
   , port = process.env.PORT || 3000;
 
-app.set('views', __dirname + '/views');
-app.engine('jade', require('jade').__express);
-app.set('view engine', 'jade');
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(require('./controllers'));
+// app.use('/api', require('././index'));
 
 app.listen(3000, function() {
   console.log('Listening on port ' + port)
-})
+});
 
---
+++ src/HelloWorld.js
@@ -0,0 +1,15 @@
+/**
+ * Created by free-lunch on 2017. 2. 4..
+ */
+
+import React from 'react';
+import {render} from 'react-dom';
+
+function HelloWorld (props) {
+  return (
+    <h1 class="header center orange-text">Hello {props.name}!</h1>
+  );
+}
+
+render(<HelloWorld name="World"/>,
+  document.getElementById('root'));
