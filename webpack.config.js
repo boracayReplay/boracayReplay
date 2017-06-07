a60647f684b23618e6add3a2d4f3697c10369d0a
JUNE\wnsgh <muhanryeon@gmail.com>
2017-06-08 00:56:46 +0900

    Ewebpack 에서 xtraTextplugin 모듈 추가 (css파일 단일로 추출 설정)

--- webpack.config.js
+++ webpack.config.js
@@ -10,17 +10,29 @@ module.exports = {
   },
   module:{
     rules:[
-          {test:/\.scss$/, use:['style-loader','css-loader','sass-loader']}
+          {
+            test:/\.scss$/,
+            use:ExtractTextPlugin.extract({
+                fallbackLoader:'style-loader',
+                loader:['css-loader','sass-loader'],
+                publicPath:'public'
+            })
+          }
     ]
   },
   plugins:[
-      new HtmlWebpackPlugin({
+    new HtmlWebpackPlugin({
         title:'gogo!', //html title 변경
         minify:{ //html을 minify 시켜줌
           collapseWhitespace:true
         },
         hash:true, //bundle.js에 hash값 추가
         template:'./src/index.html',
-    })
-  ]
+    }),
+      new ExtractTextPlugin({//css파일을 단일로 추출
+        filename:'app.css',
+        disable:false,
+        allChunks:true
+      })
+    ]
 }
